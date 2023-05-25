7720c7bbb947d771f54dcf177cac60fcfca8582a
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-05-25 15:38:29 +0200

    validering

--- routes/product.js
+++ routes/product.js
@@ -14,49 +14,69 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
 	let addProduct = req.body
 
-	await db.read()
-	addProduct.id = Math.floor(Math.random() * 100000)
-	db.data.products.push(addProduct)
-	await db.write()
-	res.send({ id: addProduct.id })
+	if (isValidProduct(addProduct)) {
 
+		await db.read()
+		addProduct.id = generateId()
+		db.data.products.push(addProduct)
+		await db.write()
+		res.send({ id: addProduct.id })
+
+	} else {
+		res.status(400).send('Invalid product info')
+	}
 })
 
 router.delete('/:id', async (req, res) => {
-    let id = Number(req.params.id)
-  
-    if (isNaN(id) || id < 0) {
-        res.sendStatus(400)
-        return
-    }
 
-    await db.read()
-    let findProduct = db.data.products.find(product => product.id === id)
-    if(!findProduct) {
-        res.sendStatus(404)
-        return
-    }
+	if(!isValidId(req.params.id)) {
+		res.status(400).send('Invalid id')
+		return
+	}
 
-    db.data.products = db.data.products.filter(product => product.id !== id)
-    await db.write()
-    res.sendStatus(200)
-  });
+	let id = Number(req.params.id)
+
+	if (isNaN(id) || id < 0) {
+		res.status(400).send('Invalid id')
+		return
+	}
+
+	await db.read()
+	let findProduct = db.data.products.find(product => product.id === id)
+	if (!findProduct) {
+		res.status(404).send('Invalid id')
+		return
+	}
+
+	db.data.products = db.data.products.filter(product => product.id !== id)
+	await db.write()
+	res.sendStatus(200)
+});
+
+
 
 router.get('/:id', async (req, res) => {
-	let id = Number(req.params.id);
-  
-	if (!isNaN(id)) {
-	  await db.read();
-	  const flower = db.data.products.find((p) => p.id === id);
-	  if (flower) {
-		res.send(flower);
-	  } else {
-		res.status(404).send('Product not found.');
-	  }
-	} else {
-	  res.status(400).send('Invalid id.');
+
+	if(!isValidId(req.params.id)) {
+		res.status(400).send('Invalid id')
+		return
 	}
-  });
+
+
+	let id = Number(req.params.id);
+
+	if (!isNaN(id)) {
+		await db.read();
+		const flower = db.data.products.find((p) => p.id === id);
+		if (flower) {
+			res.send(flower);
+		} else {
+			res.status(404).send('Product not found.');
+		}
+	} else {
+		res.status(400).send('Invalid id.');
+	}
+});
 
 
 export default router
