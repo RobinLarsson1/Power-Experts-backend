ecc2e0c921912a43fc91e53b004bfdfe08560b02
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-05-25 09:49:16 +0200

    file-update

---
+++ Data/database.js
@@ -0,0 +1,15 @@
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
+function getDb() {
+	const __dirname = dirname(fileURLToPath(import.meta.url))
+	const file = join(__dirname, 'db.json')
+	const adapter = new JSONFile(file)
+	const db = new Low(adapter, {})
+	// {} är default data
+	return db
+}
+
+export { getDb }
