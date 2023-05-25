84c540a0e1fb0561bf4ce2d8fb924878724b023c
Willi4mL <wi.lindbolm@gmail.com>
2023-05-25 15:40:29 +0200

    import search to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 import express from 'express'
 import productsRouter from './routes/product.js'
 import usersRouter from './routes/user.js'
+import searchRouter from './routes/search.js'
+import { search } from './routes/search.js'
 
 
 // Konfigurera server
@@ -14,6 +16,7 @@ app.use('/api', express.json())  // gör så att vi kan använda req.body
 // Routes
 app.use('/api/products', productsRouter)
 app.use('/api/users', usersRouter)
+app.use(`/api/${search}`, searchRouter)
 
 // starta
 app.listen(port, () => {
