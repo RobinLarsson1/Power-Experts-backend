9965188c7926b81712eca117b65e7a023bcd4064
Willi4mL <wi.lindbolm@gmail.com>
2023-05-25 15:25:30 +0200

    can post, delete and get user id

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,53 @@ const db = getDb()
 router.get('/', async (req, res) => {
 	await db.read()
 	res.send(db.data.users)
-	
 })
+
+router.post('/', async (req, res) => {
+	let addUser = req.body
+
+	await db.read()
+	addUser.id = Math.floor(Math.random() * 100000)
+	db.data.users.push(addUser)
+	await db.write()
+	res.send({ id: addUser.id })
+})
+
+router.delete('/:id', async (req, res) => {
+    let id = Number(req.params.id)
+  
+    if (isNaN(id) || id < 0) {
+        res.sendStatus(400)
+        return
+    }
+
+    await db.read()
+    let findUser = db.data.users.find(user => user.id === id)
+    if(!findUser) {
+        res.sendStatus(404)
+        return
+    }
+
+    db.data.users = db.data.users.filter(user => user.id !== id)
+    await db.write()
+    res.sendStatus(200)
+  });
+
+  router.get('/:id', async (req, res) => {
+	let id = Number(req.params.id);
+  
+	if (!isNaN(id)) {
+	  await db.read();
+	  const user = db.data.users.find((p) => p.id === id);
+	  if (user) {
+		res.send(user);
+	  } else {
+		res.status(404).send('User not found.');
+	  }
+	} else {
+	  res.status(400).send('Invalid id.');
+	}
+  });
+
 
 export default router
