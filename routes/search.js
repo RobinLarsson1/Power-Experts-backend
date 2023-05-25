13e35f9b00569df438a66df9ebcefde861b150e8
Willi4mL <wi.lindbolm@gmail.com>
2023-05-25 17:15:41 +0200

    search function works

--- routes/search.js
+++ routes/search.js
@@ -4,13 +4,22 @@ import { getDb } from '../Data/database.js'
 const router = express.Router()
 const db = getDb()
 
-router.get('/', async (req, res) => {
-	await db.read()
-	res.send(db.data.products)
-})
+router.get('/:name', async (req, res) => {
+	let name = req.params.name
 
-export const search = () => {
-	
-}
+	if((typeof name) === 'string') {
+		await db.read();
+		let findProduct = db.data.products.filter(product => product.name.toLowerCase().includes(name))
+		
+		if(findProduct) {
+			res.send(findProduct)
+			await db.write()
+		}
+		else {
+			res.send('not found')
+			await db.write()
+		}
+	}
+})
 
 export default router
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import express from 'express'
 import productsRouter from './routes/product.js'
 import usersRouter from './routes/user.js'
 import searchRouter from './routes/search.js'
-import { search } from './routes/search.js'
-
 
 // Konfigurera server
 const port = 1523
@@ -16,7 +14,7 @@ app.use('/api', express.json())  // gör så att vi kan använda req.body
 // Routes
 app.use('/api/products', productsRouter)
 app.use('/api/users', usersRouter)
-app.use(`/api/${search}`, searchRouter)
+app.use('/api/search', searchRouter)
 
 // starta
 app.listen(port, () => {
