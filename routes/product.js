07dacc77e9d130876a4c1a81f39ba1b8546ccb42
Willi4mL <wi.lindbolm@gmail.com>
2023-05-25 14:50:24 +0200

    can post

--- routes/product.js
+++ routes/product.js
@@ -7,11 +7,27 @@ const db = getDb()
 router.get('/', async (req, res) => {
 	await db.read()
 	res.send(db.data.products)
-	
+
 })
 
-router.post('/', async(req, res) => {
-	
+router.post('/', async (req, res) => {
+	let addProduct = req.body
+
+	await db.read()
+	addProduct.id = Math.floor(Math.random() * 100000)
+	db.data.products.push(addProduct)
+	await db.write()
+	res.send({ id: addProduct.id })
+
+})
+
+router.delete('/:id', async(req, res) => {
+	let id = Number(req.params.id)
+
+	await db.read()
+	db.data.products = db.data.products.filter(product => product.id !== id)
+	await db.write()
+	res.sendStatus(200)
 })
 
 export default router
