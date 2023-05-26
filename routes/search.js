8b5780078e22cda585c751a7607eeccaf7deab3e
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-05-26 09:37:38 +0200

    update

--- routes/search.js
+++ routes/search.js
@@ -1,27 +1,26 @@
-import express from 'express'
-import { getDb } from '../Data/database.js'
+import express from 'express';
+import { getDb } from '../Data/database.js';
 
-const router = express.Router()
-const db = getDb()
+const router = express.Router();
+const db = getDb();
 
-router.get('/:name', async (req, res) => {
-	let name = req.query.name
+router.get('/', async (req, res) => {
+  const name = req.query.name;
 
-	if((typeof name) === 'string') {
-		await db.read();
-		const findProduct = db.data.products.filter(product =>
+  if (typeof name === 'string') {
+    await db.read();
+    const findProduct = db.data.products.filter(product =>
       product.name.toLowerCase().includes(name.toLowerCase())
     );
-		
-		if(findProduct.length > 0 ) {
-			res.send(findProduct)
-			await db.write()
-		}
-		else {
-			res.status(404).send('not found')
-			await db.write()
-		}
-	}
-})
 
-export default router
+    if (findProduct.length > 0) {
+      res.send(findProduct);
+      await db.write();
+    } else {
+      res.status(404).send('not found');
+      await db.write();
+    }
+  }
+});
+
+export default router;
